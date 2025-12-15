d94ab8a88528a82e5a77e97e5c2b232c57f3f0f4
Siva M <sivrak22>
2025-12-15 13:47:06 +0530

    update build

--- powers/gitpush.ps1
+++ powers/gitpush.ps1
@@ -1,5 +1,10 @@
+cd E:\madmania\apps\gym\
+Write-Host "Press any key to continue..."
+npm run build
+Write-Host "Press any key to continue..."
 Copy-Item -Path 'E:\madmania\apps\gym\dist\*' -Destination 'E:\github\gymbuild' -Recurse -Force
 cd E:\github\gymbuild
+Write-Host "Press any key to continue..."
 git add .
 git commit -m "update build"
 git push
