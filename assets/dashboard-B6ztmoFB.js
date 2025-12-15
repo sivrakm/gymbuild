2403e3bd07121a93936422b33ba057e3f68173d2
Siva M <sivrak22>
2025-12-15 14:37:04 +0530

    update build

---
+++ assets/dashboard-B6ztmoFB.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-Dx1VnHVZ.js";function n(){return t.jsx(t.Fragment,{children:t.jsx("h1",{style:{color:"white"},children:"Gym App"})})}const e=()=>t.jsx(n,{});export{e as component};
