a534a019fa5c2adeacc956f37ff05d8cf8a8c1d5
Siva M <sivrak22>
2025-12-15 18:02:46 +0530

    update build

---
+++ assets/dashboard-D1436jdB.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DQTfVWzX.js";function o(){return window.location.href="https://sivrakm.github.io/demogym/dash2.html",t.jsx(t.Fragment,{})}const m=()=>t.jsx(o,{});export{m as component};
