8c073c4e7f528647014f8ba93f671d5352e2dce2
Siva M <sivrak22>
2025-12-15 16:15:47 +0530

    update build

--- powers/gitpush.ps1
+++ powers/gitpush.ps1
@@ -1,10 +1,13 @@
 cd E:\madmania\apps\gym\
 Write-Host "Press any key to continue..."
+$x = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 npm run build
 Write-Host "Press any key to continue..."
+$x = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 Copy-Item -Path 'E:\madmania\apps\gym\dist\*' -Destination 'E:\github\gymbuild' -Recurse -Force
 cd E:\github\gymbuild
 Write-Host "Press any key to continue..."
+$x = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 git add .
 git commit -m "update build"
 git push
