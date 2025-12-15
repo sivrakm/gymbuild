812c100f1abd95db9281b0997b5d8fb3792a2d85
Siva M <sivrak22>
2025-12-15 16:02:38 +0530

    update build

---
+++ assets/dashboard-Bc534IHO.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-Dmwq1RYR.js";function n(){return t.jsx(t.Fragment,{children:t.jsx("h1",{style:{color:"white"},children:"Gym App"})})}const e=()=>t.jsx(n,{});export{e as component};
