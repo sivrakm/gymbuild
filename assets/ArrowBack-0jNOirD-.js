6c43424dee0f0203ceeb5e274d2923a079f74807
Siva M <sivrak22>
2025-12-31 01:46:39 +0530

    update build

---
+++ assets/ArrowBack-0jNOirD-.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-D30F43m9.js";import{c as r}from"./Box-DH9TF4i8.js";const a=r(o.jsx("path",{d:"M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20z"}));export{a as A};
