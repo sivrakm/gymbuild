b7a369f478e2d492a20c845cbf2d8db4b0db542e
Siva M <sivrak22>
2025-12-31 14:02:18 +0530

    update build

---
+++ assets/ArrowBack-BGRd6IhK.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-BFGe_F2p.js";import{d as r}from"./Button-DuPQX7-9.js";const a=r(o.jsx("path",{d:"M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20z"}));export{a as A};
---
+++ assets/CheckCircle-Bn7H_D-o.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-BFGe_F2p.js";import{d as r}from"./Button-DuPQX7-9.js";const e=r(o.jsx("path",{d:"M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2m-2 15-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8z"}));export{e as C};
